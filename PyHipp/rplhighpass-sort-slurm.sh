3d117079430277e57cc878e8d3a481aa4ca7988c
zzzzaaaacccc <a.zacharie@u.nus.edu>
2025-09-05 08:22:11 +0000

    Lab 5 Part 3 Step 16

--- PyHipp/rplhighpass-sort-slurm.sh
+++ PyHipp/rplhighpass-sort-slurm.sh
@@ -13,6 +13,11 @@
 #SBATCH -e rplhighpasssort-slurm.%N.%j.err # STDERR
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
 python -u -c "import PyHipp as pyh; \
 import time; \
 pyh.RPLHighPass(saveLevel=1); \
@@ -21,3 +26,7 @@ mountain_batch.mountain_batch(); \
 from PyHipp import export_mountain_cells; \
 export_mountain_cells.export_mountain_cells(); \
 print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
+
